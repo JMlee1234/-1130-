ac167bba9f42c9b1150322f6782fc395d045771b
JMlee1234 <jungmin2244@naver.com>
2021-11-30 13:30:16 +0900

    반복문연습

---
+++ DemoLoop3.py
@@ -0,0 +1,15 @@
+# DemoLoop3.py
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+
+for i in lst:
+    if i > 5:
+        break
+    print("Item:{0}".format(i))
+
+print("---continue---")
+for i in lst:
+    if i % 2 == 1:
+        continue
+    print("Item:{0}".format(i))
+
